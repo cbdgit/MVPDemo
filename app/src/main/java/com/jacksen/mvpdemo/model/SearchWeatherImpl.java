bfd4e5e7b880906f946bc5bcb1fa32bbcd79fb8a
yansen <951532416@qq.com>
2016-04-21 15:54:05 +0800

    update mvp

--- app/src/main/java/com/jacksen/mvpdemo/model/SearchWeatherImpl.java
+++ app/src/main/java/com/jacksen/mvpdemo/model/SearchWeatherImpl.java
@@ -1,5 +1,11 @@
 package com.jacksen.mvpdemo.model;
 
+import android.os.Handler;
+import android.os.Message;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.jacksen.mvpdemo.bean.WeatherInfo;
 import com.jacksen.mvpdemo.presenter.WeatherPresenter;
 import com.jacksen.mvpdemo.util.Constants;
@@ -22,42 +28,70 @@ public class SearchWeatherImpl implements SearchWeatherModel {
     }
 
     @Override
-    public WeatherInfo searchWeather(String cityName) {
-        String jsonResult = requestWeatherInfo(Constants.API_URL, "city=" + cityName);
-//        Log.d("SearchWeatherImpl", jsonResult);
-        System.out.println(jsonResult);
-        return null;
+    public void searchWeather(String cityName) {
+        requestWeatherInfo(Constants.API_URL + "?city=" + cityName);
     }
 
     /**
      * @param httpUrl
-     * @param cityName
      * @return
      */
-    public String requestWeatherInfo(String httpUrl, String cityName) {
-        BufferedReader reader = null;
-        String result = "";
-        StringBuffer sb = new StringBuffer();
-        httpUrl = httpUrl + "?" + cityName;
-        try {
-            URL url = new URL(httpUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("apikey", Constants.API_KEY);
-            connection.connect();
-            InputStream inputStream = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-            String strRead = null;
-            while ((strRead = reader.readLine()) != null) {
-                sb.append(strRead);
-                sb.append("\r\n");
-            }
-            reader.close();
-            result = sb.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void requestWeatherInfo(final String httpUrl) {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                BufferedReader reader = null;
+                String result = "";
+                StringBuffer sb = new StringBuffer();
+                try {
+                    URL url = new URL(httpUrl);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("apikey", Constants.API_KEY);
+                    connection.connect();
+                    InputStream inputStream = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    String strRead = null;
+                    while ((strRead = reader.readLine()) != null) {
+                        sb.append(strRead);
+                        sb.append("\r\n");
+                    }
+                    reader.close();
+                    result = sb.toString();
 
-        return result;
+                    Message msg = Message.obtain();
+                    msg.what = 1;
+                    msg.obj = result;
+                    handler.sendMessage(msg);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Message msg = Message.obtain();
+                    msg.what = 2;
+                    handler.sendMessage(msg);
+                } finally {
+
+                }
+            }
+        }.start();
     }
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    JSONObject jsonObject = JSON.parseObject((String) msg.obj);
+                    JSONArray array = jsonObject.getJSONArray(Constants.SuperJsonObject);
+
+                    WeatherInfo weatherInfo = JSON.parseObject(((JSONObject) array.get(0)).toJSONString(), WeatherInfo.class);
+                    weatherPresenter.loadDataSuccess(weatherInfo);
+                    break;
+                case 2:
+                    weatherPresenter.loadDataFailure("加载失败，请重试!");
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/com/jacksen/mvpdemo/model/SearchWeatherModel.java
+++ app/src/main/java/com/jacksen/mvpdemo/model/SearchWeatherModel.java
@@ -1,7 +1,5 @@
 package com.jacksen.mvpdemo.model;
 
-import com.jacksen.mvpdemo.bean.WeatherInfo;
-
 /**
  * Created by Admin on 2016/4/21.
  */
@@ -11,5 +9,5 @@ public interface SearchWeatherModel {
      * @param cityName
      * @return
      */
-    WeatherInfo searchWeather(String cityName);
+    void searchWeather(String cityName);
 }
